8ccda7b31b45f4c78d22eb7692c951a368c94713
ggh93 <ggh93@fandomberry.com>
2022-08-03 21:39:10 +0900

    feat: 1일차 라우터설정

---
+++ src/core/router.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Routers, Route, Routes } from 'react-router-dom';
+
+import TestPage from '../pages/TestPage';
+import MainPage from '../pages/MainPage';
+
+function Router() {
+	return (
+		<Routers>
+			<Routes>
+				<Route path="/" element={<MainPage />} />
+				<Route path="/test" element={<TestPage />} />
+			</Routes>
+		</Routers>
+	);
+}
+
+export default Router;
---
+++ src/pages/TestPage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function TestPage() {
+	console.log('1');
+	return <div>TestPage</div>;
+}
